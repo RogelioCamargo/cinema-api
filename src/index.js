17c02ed430c20eeafb44f00c3ef2ea17e5845e47
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-01-11 08:31:49 -0800

    Connnet to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const { ApolloServer, gql } = require("apollo-server");
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 const { typeDefs, resolvers } = require("./schema");
+
+mongoose.connect(process.env.MONGODB_URI_TEST)
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((error) => console.log("Error Connecting to MongoDB", error.message));
 
 const server = new ApolloServer({
 	typeDefs,
