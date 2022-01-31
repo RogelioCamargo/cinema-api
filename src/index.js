250a4f95aca630c326d5cdc0823d3524dff85791
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-01-31 11:31:13 -0800

    Fix heroku deployment

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const server = new ApolloServer({
 	resolvers
 });
 
-server.listen().then(() => {
-	console.log("🚀Server Ready!🚀")
-});
+const port = process.env.PORT || "4000";
+
+server.listen(port);
+console.log(`🚀Server Ready!🚀`);
