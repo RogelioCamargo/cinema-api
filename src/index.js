aa2f0cad99c08584e7dfb09043a6fa2d3429112e
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-01-31 21:37:55 -0800

    Make mongodb connection using async/await

--- src/index.js
+++ src/index.js
@@ -4,33 +4,41 @@ const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 const { typeDefs, resolvers } = require("./schema");
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log("Connected to MongoDB"))
-.catch((error) => console.log("Error Connecting to MongoDB", error.message));
+// mongoose.connect(process.env.MONGODB_URI)
+// .then(() => console.log("Connected to MongoDB"))
+// .catch((error) => console.log("Error Connecting to MongoDB", error.message));
 
 const express = require("express");
 const http = require("http");
 const cors = require("cors");
 
 (async function () {
-	const app = express();
-	app.use(cors());
-	const httpServer = http.createServer(app);
-
-	const server = new ApolloServer({
-		typeDefs,
-		resolvers,
-		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-	});
-
-	await server.start();
-	server.applyMiddleware({
-		app,
-		path: "/"
-	})
-
-	const PORT = process.env.PORT || "4000";
-	httpServer.listen(PORT, () => {
-		console.log(`🚀Server Ready!🚀`);
-	});
+	try {
+		await mongoose.connect(process.env.MONGODB_URI);
+		console.log("Connected to MongoDB");
+		
+		const app = express();
+		app.use(cors());
+		app.use(express.json());
+		const httpServer = http.createServer(app);
+	
+		const server = new ApolloServer({
+			typeDefs,
+			resolvers,
+			plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+		});
+	
+		await server.start();
+		server.applyMiddleware({
+			app,
+			path: "/"
+		})
+	
+		const PORT = process.env.PORT || "4000";
+		httpServer.listen(PORT, () => {
+			console.log(`🚀Server Ready!🚀`);
+		});
+	} catch (error) {
+		console.log(error.message);
+	}
 })();
